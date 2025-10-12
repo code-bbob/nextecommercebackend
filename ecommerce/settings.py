3fe505ef9c7443ea9c5d2bd82e3a8961d32827e0
code-bbob <officialbibhab@gmail.com>
2025-10-12 16:41:12 +0545

    fix: simplify database configuration logic for production settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -97,14 +97,14 @@ PRODUCTION = os.environ.get('PRODUCTION', 'False') == 'True'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if PRODUCTION == 'False':
+if not PRODUCTION:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-elif PRODUCTION == 'True':
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
